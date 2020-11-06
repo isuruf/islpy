0cc36649c4b66a8eb4c68d239953a310e685794b
Isuru Fernando <isuruf@gmail.com>
2020-11-06 14:52:00 -0600

    debug mac

--- wheels/config.sh
+++ wheels/config.sh
@@ -19,6 +19,6 @@ function run_tests {
 function pip_wheel_cmd {
     local abs_wheelhouse=$1
     pip install pybind11
-    pip wheel $(pip_opts) -w $abs_wheelhouse --no-deps .
+    pip wheel $(pip_opts) -w $abs_wheelhouse --no-deps . || (clang -DNDEBUG -fwrapv -Wall -arch x86_64 -I/usr/local/include -DUSE_IMATH_FOR_MP=1 -DUSE_SMALL_INT_OPT=1 -Isrc/wrapper -Iisl-supplementary -Iisl/include -Iisl -Iisl/imath -I/Users/runner/work/islpy/islpy/venv/lib/python3.6/site-packages/pybind11/include -I/Users/runner/work/islpy/islpy/venv/lib/python3.6/site-packages/pybind11/include -I/Users/runner/work/islpy/islpy/venv/include -I/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m -c -std=gnu++14 src/wrapper/wrap_isl_part1.cpp -fvisibility=hidden -DVERSION_INFO="2020.2.2" -fvisibility=hidden -stdlib=libc++ -mmacosx-version-min=10.7 -E)
 }
 
